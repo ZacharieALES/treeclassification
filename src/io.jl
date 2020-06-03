390b36b2d73c37af09f83aabd1eff54652710401
Rapoyo <qrapilly@gmail.com>
2020-06-03 12:17:44 +0200

    premiers tests de l'algo

--- src/io.jl
+++ src/io.jl
@@ -66,3 +66,13 @@ function Tree(D::Int64,a::Array{Float64,2},b::Array{Float64,1},c::Array{Int64,1}
     this.c=c
     return(this)
 end
+
+
+function null_Tree()
+    this=Tree()
+    this.D=0
+    this.a=zeros(Float64,0,0)
+    this.b=zeros(Float64,0)
+    this.c=zeros(Int64,0)
+    return(this)
+end
--- src/resolution.jl
+++ src/resolution.jl
@@ -33,10 +33,9 @@ function find_p(x::Int64)
 end 
 
 
-function classification_tree_MIO(D::Int64,N_min::Int64,C::Int64,X::Array{Float64,2},Y::Array{Int64,1},K::Int64,warm_start::Tree=nothing,H::Bool=false,alpha::Float64=0)
+function classification_tree_MIO(D::Int64,N_min::Int64,C::Int64,X::Array{Float64,2},Y::Array{Int64,1},K::Int64,warm_start::Tree=null_Tree(),H::Bool=false,alpha::Float64=0.0)
     n=length(Y)
-    p=length(X[1])
-
+    p=length(X[1,:])
     Yk=-ones(Int64,n,K)
 
     for i in 1:n
@@ -45,7 +44,7 @@ function classification_tree_MIO(D::Int64,N_min::Int64,C::Int64,X::Array{Float64
 
     eps=ones(Float64,p)
     for i_1 in 1:n
-        for i_2 in i_1:n
+        for i_2 in i_1+1:n
             for j in 1:p
                 diff=abs(X[i_1,j]-X[i_2,j])
                 if diff>0 && diff<eps[j]
@@ -54,6 +53,7 @@ function classification_tree_MIO(D::Int64,N_min::Int64,C::Int64,X::Array{Float64
             end
         end
     end
+
 
     eps_max=0
     for j in 1:p
@@ -67,74 +67,87 @@ function classification_tree_MIO(D::Int64,N_min::Int64,C::Int64,X::Array{Float64
         Count_class[Y[i]]+=1
     end
 
-    L_hat=max(Count_class)
+    println(eps)
+    println(eps_max)
+    
 
-    m=Model(with_optimizer(CPLEX.Optimizer))
+    L_hat=Count_class[1]
+    for k in 2:K
+        if Count_class[k]>L_hat
+            L_hat=Count_class[k]
+        end
+    end 
+
+    model=Model(with_optimizer(CPLEX.Optimizer))
 
     n_l=2^D #Leaves number
     n_b=2^D-1 #Branch nodes number
 
-    @variable(m, L[1:n_l],Int)
-    @variable(m, N[1:n_l],Int)
-    @variable(m, N_k[1:K,1:n_l],Int)
-    @variable(m, c[1:K,1:n_l],Bin)
-    @variable(m, z[1:n,1:n_l],Bin)
+    @variable(model, L[1:n_l],Int)
+    @variable(model, N[1:n_l],Int)
+    @variable(model, N_k[1:K,1:n_l],Int)
+    @variable(model, c[1:K,1:n_l],Bin)
+    @variable(model, z[1:n,1:n_l],Bin)
     if H
-        @variable(m, a[1:n_b,1:p],Bin)
+        @variable(model, a[1:n_b,1:p],Float)
+        @variable(model, hat_a[1:n_b,1:p],Float)
+        @variable(model, s[1:n_b,1:p],Bin)
     else
-        @variable(m, a[1:n_b,1:p],Float)
-        @variable(m, hat_a[1:n_b,1:p],Float)
-        @variable(m, s[1:n_b,1:p],Bin)
+        @variable(model, a[1:n_b,1:p],Bin)
     end
-    @variable(m, b[1:n_b],Float)
-    @variable(m, d[1:n_b],Bin)
-    @variable(m, l[1:n_b],Bin)
+    @variable(model, b[1:n_b]>=0)  #vérifier que c'est bien un float puis faire pareil pour les autres
+    @variable(model, d[1:n_b],Bin)
+    @variable(model, l[1:n_l],Bin)
 
 
-    @constraint(m, [k in 1:K, t in 1:n_l], L[t] >= N[t]-N_k[k,t]-n*(1-c[k,t]))
-    @constraint(m, [k in 1:K, t in 1:n_l], L[t] <= N[t]-N_k[k,t]+n*c[k,t])
-    @constraint(m, [t in 1:n_l], L[t]>=0)
-    @constraint(m, [k in 1:K, t in 1:n_l], N_k[k,t] == (1/2)*(sum((1+Yk[i,k])*z[i,t] for i in 1:n)))
-    @constraint(m, [t in 1:n_l], N[t] == sum(z[it] for i in 1:n))
-    @constraint(m, [t in 1:n_l], l[t] == sum(c[kt] for k in 1:K))
+    @constraint(model, [k in 1:K, t in 1:n_l], L[t] >= N[t]-N_k[k,t]-n*(1-c[k,t]))
+    @constraint(model, [k in 1:K, t in 1:n_l], L[t] <= N[t]-N_k[k,t]+n*c[k,t])
+    @constraint(model, [t in 1:n_l], L[t]>=0)
+    @constraint(model, [k in 1:K, t in 1:n_l], N_k[k,t] == (1/2)*(sum((1+Yk[i,k])*z[i,t] for i in 1:n)))
+    @constraint(model, [t in 1:n_l], N[t] == sum(z[i,t] for i in 1:n))
+    @constraint(model, [t in 1:n_l], l[t] == sum(c[k,t] for k in 1:K))
         
     mu=0.005
 
     for t in 1:n_l
         (Al,Ar)=find_Al_Ar(t+n_b)
+        print("t = ")
+        println(t)
+        println(Al)
+        println(Ar)
         if H
-            @constraint(m, [i in 1:n, m in Ar], sum(a[m,j]*x[i,j] for j in 1:p) >= b[m]-2*(1-z[i,t]))
-            @constraint(m, [i in 1:n, m in Al], sum(a[m,j]*x[i,j] for j in 1:p) +mu <= b[m]+(2+mu)*(1-z[i,t]))
+            @constraint(model, [i in 1:n, m in Ar], sum(a[m,j]*X[i,j] for j in 1:p) >= b[m]-2*(1-z[i,t]))
+            @constraint(model, [i in 1:n, m in Al], sum(a[m,j]*X[i,j] for j in 1:p) +mu <= b[m]+(2+mu)*(1-z[i,t]))
         else
-            @constraint(m, [i in 1:n, m in Ar], sum(a[m,j]*x[i,j] for j in 1:p) >= b[m]-(1-z[i,t]))
-            @constraint(m, [i in 1:n, m in Al], sum(a[m,j]*(x[i,j]+eps[j]) for j in 1:p) <= b[m]+(1+eps_max)*(1-z[i,t]))
+            @constraint(model, [i in 1:n, m in Ar], sum(a[m,j]*X[i,j] for j in 1:p) >= b[m]-(1-z[i,t]))
+            @constraint(model, [i in 1:n, m in Al], sum(a[m,j]*(X[i,j]+eps[j]) for j in 1:p) <= b[m]+(1+eps_max)*(1-z[i,t]))
         end
     end
 
-    @constraint(m, [i in 1:n], sum(z[i,t] for t in 1:n_l)==1)
-    @constraint(m, [i in 1:n, t in 1:n_l], z[i,t]<=l[t])
-    @constraint(m, [t in 1:n_l], sum(z[i,t] for i in 1:n) >= N_min*l[t])
-    @constraint(m, [t in 1:n_b], sum(a[t,j] for j in 1:p)==d[t])
+    @constraint(model, [i in 1:n], sum(z[i,t] for t in 1:n_l)==1)
+    @constraint(model, [i in 1:n, t in 1:n_l], z[i,t]<=l[t])
+    @constraint(model, [t in 1:n_l], sum(z[i,t] for i in 1:n) >= N_min*l[t])
+    @constraint(model, [t in 1:n_b], sum(a[t,j] for j in 1:p)==d[t])
     if H
-        @constraint(m, [t in 1:n_b], -d[t] <= b[t])
+        @constraint(model, [t in 1:n_b], -d[t] <= b[t])
     else
-        @constraint(m, [t in 1:n_b], 0<= b[t])
+        @constraint(model, [t in 1:n_b], 0<= b[t])
     end
-    @constraint(m, [t in 1:n_b], b[t]<= d[t])
-    @constraint(m, [t in 2:n_b], d[t]<=d[find_p(t)])
+    @constraint(model, [t in 1:n_b], b[t]<= d[t])
+    @constraint(model, [t in 2:n_b], d[t]<=d[find_p(t)])
 
     if H
-        @constraint(m, [t in 1:n_b], sum(hat_a[j,t] for j in 1:p) <= d[t])
-        @constraint(m,[j in 1:p,t in 1:n_b],a[j,t]<=hat_a[j,t])
-        @constraint(m,[j in 1:p,t in 1:n_b],-a[j,t]<=hat_a[j,t])
+        @constraint(model, [t in 1:n_b], sum(hat_a[j,t] for j in 1:p) <= d[t])
+        @constraint(model,[j in 1:p,t in 1:n_b],a[j,t]<=hat_a[j,t])
+        @constraint(model,[j in 1:p,t in 1:n_b],-a[j,t]<=hat_a[j,t])
 
-        @constraint(m,[j in 1:p,t in 1:n_b],-s[j,t]<=a[j,t])
-        @constraint(m,[j in 1:p,t in 1:n_b],a[j,t]<=s[j,t])
-        @constraint(m,[j in 1:p,t in 1:n_b],s[j,t]<=d[t])
-        @constraint(m, [t in 1:n_b], sum(s[j,t] for j in 1:p) >= d[t])
+        @constraint(model,[j in 1:p,t in 1:n_b],-s[j,t]<=a[j,t])
+        @constraint(model,[j in 1:p,t in 1:n_b],a[j,t]<=s[j,t])
+        @constraint(model,[j in 1:p,t in 1:n_b],s[j,t]<=d[t])
+        @constraint(model, [t in 1:n_b], sum(s[j,t] for j in 1:p) >= d[t])
     end
 
-    if warm_start !== nothing
+    if warm_start.D !=0
         D_tree=warm_start.D
         n_b_tree=2^D_tree-1
         n_l_tree=2^D_tree
@@ -143,30 +156,37 @@ function classification_tree_MIO(D::Int64,N_min::Int64,C::Int64,X::Array{Float64
     end
 
     if H
-        @objective(m, Min, (1/L_hat)*sum(L[t] for t in 1:n_l) + alpha*sum(sum(s[j,t] for j in 1:p) for t in 1:n_b))
+        @objective(model, Min, (1/L_hat)*sum(L[t] for t in 1:n_l) + alpha*sum(sum(s[j,t] for j in 1:p) for t in 1:n_b))
     else
-        @constraint(m, sum(d[t] for t in 1:n_b)<=C)
-        @objective(m, Min, (1/L_hat)*sum(L[t] for t in 1:n_l))
+        @constraint(model, sum(d[t] for t in 1:n_b)<=C)
+        @objective(model, Min, (1/L_hat)*sum(L[t] for t in 1:n_l))
     end
 
-    final_c=zeros(Int64,n)
+    optimize!(model)
+
+    final_c=zeros(Int64,n_l)
     n_l=2^D
     for t in 1:n_l
-        k=1
-        while value(m.c[k,t])!=1
-            k+1
+        if value(l[t])==1
+            k=1
+            while value(c[k,t])!=1
+                k=k+1
+            end
+            final_c[t]=k
+        else
+            final_c[t]=0
         end
-        final_c[i]=k
     end
 
-    return(Tree(D,value.(m.a),value.(m.b),final_c   ))
+    return(Tree(D,value.(a),value.(b),final_c))
+
 end
 
 function score(T::Tree,X::Array{Float64,2},Y::Array{Int64,1})
     s=0
     D=T.D
     n=length(Y)
-    p=length(X[1])
+    p=length(X[1,:])
     max=2^D
     for i in 1:n
         if predict_class(T,X[i,:])!=Y[i]
@@ -177,7 +197,7 @@ function score(T::Tree,X::Array{Float64,2},Y::Array{Int64,1})
 end
 
 
-function indice_best_tree(warm_start_list::Array{(Tree,Int64),1})
+function indice_best_tree(warm_start_list::Array{Tuple{Tree,Int64},1})
     i_min=1
     min=warm_start_list[1,2]
     for i in 2:length(warm_start_list)
@@ -189,7 +209,7 @@ function indice_best_tree(warm_start_list::Array{(Tree,Int64),1})
     return(i_min)
 end
 
-function OCT(D_max::Int64,N_Min::Int64,X::Array{Float64,2},Y::Array{Int64,1},K::Int64,H::Bool=false,alpha::Array{Float64,1})
+function OCT(D_max::Int64,N_Min::Int64,X::Array{Float64,2},Y::Array{Int64,1},K::Int64,H::Bool=false,alpha::Array{Float64,1}=[0])
     warm_start_list=[]
     n=length(Y)
     for D in 1:D_max
